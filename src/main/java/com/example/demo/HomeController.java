2392f406212e286db075db4a96093a109fc48305
kouamax15 <kouamax15@gmail.com>
2019-08-23 10:36:59 -0400

    excercise 103_hello world external variables

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,8 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
+
         @RequestMapping("/")
-        public String homePage (){
+        public String homePage () {
                 return "hometemplate";
         }
 }
